71093fd84390413e386d458eaa78cda3584984d9
Glenn Clapp <clappg@jsjcorp.com>
2020-03-04 18:45:26 -0500

    specify tab names

--- Tdms_file_converter.py
+++ Tdms_file_converter.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import nptdms 
 import csv
-import pip
+import os
 
 
 
 def tdms_to_dfs(file_path):
-    '''Accepts a path to a *.tdms file as an input and creates a list of pandas
-    data frames with that file. Where each channel in the tdms file is a
-    separate dataframe.'''
+    '''Accepts a path to a *.tdms file as an input and creates a list of
+    tuples where the first element in each tuple is a channel name, and the
+    second is a pandas data frame with data from that channel.'''
     
     f = nptdms.TdmsFile(file_path)
     
@@ -17,18 +17,19 @@ def tdms_to_dfs(file_path):
     DF_list = [] # initialize list to store dataframes
     for c in channels:
         new_DF = f.object(c).as_dataframe()
-        DF_list.append(new_DF)
+        DF_list.append((c,new_DF))
     
     return(DF_list)
 
 def dfs_to_excel(DF_list):
-    '''Accepts a list of pandas data frames and outputs an excel file where each
-    dataframe is made a tab in the excel file.'''
+    '''Accepts a list of tuples where the first element is a sheet name and the
+    second is a pandas data frame, and outputs an excel file where each
+    dataframe is made a tab in the excel file with the name given.'''
+    
     with pd.ExcelWriter('output.xlsx') as writer:
-        for i, df in enumerate(DF_list):
-            df.to_excel(writer,sheet_name=str(i))
+        for df in DF_list:
+            df[1].to_excel(writer,df[0])
 
 if __name__ == '__main__':
-    pip.install('openpyxl')
     DF_list = tdms_to_dfs(r'E:\Work\GHSP\HDrive\WIP\12504 - LD Police IP shifter\Issue #314 - Pursuit design validation testing\Issue #314.5 - DVPV-124 Sensor drift at 40 and -85C\202000435 - Pivot support test\202000435 Generic Loading.is_ccyclic_RawData\S0007167_Rearward Loading 222N_N_A_20200227131547.tdms')
     dfs_to_excel(DF_list)
