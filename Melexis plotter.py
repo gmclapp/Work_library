e9a435c2769e82a78192d0af100efc81b7bcffb1
Glenn Clapp <clappg@jsjcorp.com>
2020-03-05 13:49:43 -0500

    Add logic to remove diagnostic data points

--- Melexis plotter.py
+++ Melexis plotter.py
@@ -10,6 +10,7 @@ ax.set_xlabel("X")
 ax.set_ylabel("Y")
 
 # Define gear transitions
+P_over = 95
 P_to_R = 58
 R_to_N = 42
 N_to_D = 25
@@ -17,15 +18,27 @@ N_to_D = 25
 D_to_N = 35
 N_to_R = 50
 R_to_P = 75
+D_over = 5
 
 CAN_data = DF_list[1][1]
 
-X = CAN_data['X1'].tolist()
-Y = CAN_data['Y1'].tolist()
+X = CAN_data['X_AVG'].tolist()
+Y = CAN_data['Y_AVG'].tolist()
+
+X = list(filter(lambda a: a != 0,X))
+X = list(filter(lambda a: a != 131.07,X))
+
+Y = list(filter(lambda a: a != 0,Y))
+Y = list(filter(lambda a: a != 131.07,Y))
+
 
 ##CAN_data.plot(kind='scatter',x='X1',y='Y1',alpha=1)
 
-ax.plot(X,Y)
+for i in range(len(X)):
+    ax.plot(X[i:i+2],Y[i:i+2],color='black')
+    
+##ax.plot(X,Y)
+ax.hlines(P_over,35,65,'black','solid')
 ax.hlines(P_to_R,35,65,'black','solid')
 ax.hlines(R_to_N,35,65,'black','solid')
 ax.hlines(N_to_D,35,65,'black','solid')
@@ -33,6 +46,10 @@ ax.hlines(N_to_D,35,65,'black','solid')
 ax.hlines(D_to_N,35,65,'black','solid')
 ax.hlines(N_to_R,35,65,'black','solid')
 ax.hlines(R_to_P,35,65,'black','solid')
+ax.hlines(D_over,35,65,'black','solid')
+
+ax.vlines(35,P_over,D_over,'black','solid')
+ax.vlines(65,P_over,D_over,'black','solid')
 
 plt.show()
 print(CAN_data.head())
