6e6ab9b26291647be1f5864ea30be80c5da612cb
Glenn Clapp <clappg@jsjcorp.com>
2021-05-19 11:56:02 -0400

    Solve pull out of park static

--- LeverFBD.py
+++ LeverFBD.py
@@ -1,18 +1,17 @@
-import math
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-def RotateXY(vector,deg):
-    '''vector = [x,y], degrees = rotation
+def RotateZ(vector,deg):
+    '''vector = numpy array [x,y,z], degrees = rotation
     Returns a unit vector rotated counter clockwise about z by degrees.'''
-    x = vector[0]
-    y = vector[0]
-    r = deg * math.pi/180
-    mag = (x**2 + y **2)**0.5
-    xprime = (x*math.cos(r)-y*math.sin(r))/mag
-    yprime = (x*math.sin(r)+y*math.cos(r))/mag
-    return([xprime,yprime])
+    r = deg * np.pi/180
+    mag = np.linalg.norm(vector)
+    R = np.array([[np.cos(r),-1*np.sin(r),0],
+                    [np.sin(r),np.cos(r),0],
+                    [0,0,1]])
+    vector_new = np.matmul(R,vector)/mag
+    return(vector_new)
 
 class leverModel:
     ''' The lever coordinate system is defined such that the positive z axis
@@ -22,11 +21,11 @@ class leverModel:
     the y axis completes the right-handed coordinate system.'''
     def __init__(self):
         # Define force application locations
-        self.cable_pin_r = [56.25,-41.66] # mm
-        self.pawl_pin_r = [69.32,0] # mm
-        self.knob_r = [238,0] # mm
-        self.detent_r = [71.15,26.39] # mm
-        self.lockplate_r = [12.7,73.92]
+        self.cable_pin_r = np.array([56.25,-41.66,0]) # mm
+        self.pawl_pin_r = np.array([69.32,0,0]) # mm
+        self.knob_r = np.array([238,0,0]) # mm
+        self.detent_r = np.array([71.15,26.39,0]) # mm
+        self.lockplate_r = np.array([12.7,73.92,0])
 
         # Define force vectors
         self.cable_pin_F = 0 # N
@@ -44,20 +43,15 @@ class leverModel:
         # Normal operation
 
     def solve_poop_static(self):
-##        x = self.cable_pin_r[0]
-##        y = self.cable_pin_r[1]
-##        mag = (x**2 + y ** 2)**0.5
-##        theta = 1.5*math.pi
-##        self.cable_pin_F = [(x*math.cos(theta)-y*math.sin(theta))*self.POOP_maxF/mag,
-##                            (x*math.sin(theta)+y*math.cos(theta))*self.POOP_maxF/mag]
-        self.cable_pin_F = RotateXY(self.cable_pin_r,270)
-        #self.cable_pin_F *= self.POOP_maxF
+        self.cable_pin_F = RotateZ(self.cable_pin_r,270)
+        self.cable_pin_F *= self.POOP_maxF
         print(self.cable_pin_F)
+        self.poop_T = np.cross(self.cable_pin_r,self.cable_pin_F)
+        print(self.poop_T)
 
-        LAPx = self.knob_r[0]
-        LAPy = self.knob_r[1]
-        LAPmag = (LAPx**2 + LAPy ** 2)**0.5
-        theta = 0.5*math.pi
+        self.knob_F = -1 * np.cross(self.poop_T,self.knob_r)/np.dot(self.knob_r,self.knob_r)
+        print(self.knob_F)
+
 
 if __name__ == "__main__":
     Lever = leverModel()
